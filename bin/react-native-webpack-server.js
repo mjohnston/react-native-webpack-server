6c29b65595555857ce5aed30933a226583b6efec
James Lal <james@silklabs.com>
2015-09-09 12:19:32 -0700

    Add bundle command and fix race

--- bin/react-native-webpack-server.js
+++ bin/react-native-webpack-server.js
@@ -4,9 +4,10 @@ var path = require('path');
 var fs = require('fs');
 var parser = require('nomnom');
 var Server = require('../lib/Server');
+var fetch = require('../lib/fetch');
 
-parser.command('start')
-  .option('hostname', {
+function commonOptions (command) {
+  return command.option('hostname', {
     default: 'localhost',
   })
   .option('port', {
@@ -23,22 +24,52 @@ parser.command('start')
   })
   .option('webpackConfigPath', {
     default: 'webpack.config.js',
-  })
+  });
+}
+
+function startServer(opts) {
+  opts.webpackConfigPath = path.resolve(process.cwd(), opts.webpackConfigPath);
+  if (fs.existsSync(opts.webpackConfigPath)) {
+    opts.webpackConfig = require(path.resolve(process.cwd(), opts.webpackConfigPath));
+  } else {
+    throw new Error('Must specify webpackConfigPath or create ./webpack.config.js');
+    process.exit(1);
+  }
+  delete opts.webpackConfigPath;
+
+  var server = new Server(opts);
+  return server;
+}
+
+commonOptions(parser.command('start')
   .option('hot', {
     flag: true,
     default: false,
-  })
+  }))
   .callback(function(opts) {
-    opts.webpackConfigPath = path.resolve(process.cwd(), opts.webpackConfigPath);
-    if (fs.existsSync(opts.webpackConfigPath)) {
-      opts.webpackConfig = require(path.resolve(process.cwd(), opts.webpackConfigPath));
-    } else {
-      throw new Error('Must specify webpackConfigPath or create ./webpack.config.js');
-      process.exit(1);
-    }
-    delete opts.webpackConfigPath;
+    var server = startServer(opts);
+    server.start();
+  });
 
-    (new Server(opts)).start();
+commonOptions(parser.command('bundle'))
+  .callback(function(opts) {
+    var server = startServer(opts);
+    var url = 'http://localhost:' + opts.port + '/index.ios.bundle';
+    var targetPath = path.resolve('./iOS/main.jsbundle');
+
+    server.start();
+
+    fetch(url).then(function(content) {
+      fs.writeFileSync(targetPath, content);
+      server.stop();
+
+      // XXX: Hack something is keeping the process alive but we can still
+      // safely kill here without leaving processes hanging around...
+      process.exit(0);
+    }).catch(function(err) {
+      console.log('Error creating bundle...', err.stack);
+      server.stop();
+    });
   });
 
 parser.parse();
