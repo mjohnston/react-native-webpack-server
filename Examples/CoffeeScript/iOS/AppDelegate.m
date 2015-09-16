8371af09d5cbd45d1e76e835891fe249ec0baca7
Dan Abramov <dan.abramov@gmail.com>
2015-09-16 22:10:58 +0300

    Fix RCTRootView.initWithBundleURL signature in examples for RN 0.11

--- Examples/CoffeeScript/iOS/AppDelegate.m
+++ Examples/CoffeeScript/iOS/AppDelegate.m
@@ -38,6 +38,7 @@
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"CoffeeScript"
+                                               initialProperties:nil
                                                    launchOptions:launchOptions];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
