410c013bbb45e9857b04db7d03e88e37711a3285
Spencer Elliott <me@elliottsj.com>
2015-10-11 13:28:11 -0400

    Use elliottsj/webpack#web-worker-hmr for HMR support in BabelES6

--- Examples/BabelES6/webpack.config.js
+++ Examples/BabelES6/webpack.config.js
@@ -6,6 +6,8 @@ var webpack = require('webpack');
 
 var config = {
 
+  target: 'webworker',
+
   debug: true,
 
   devtool: 'source-map',
@@ -41,12 +43,18 @@ var config = {
 
 // Hot loader
 if (process.env.HOT) {
-  config.devtool = 'eval'; // Speed up incremental builds
-  config.entry['index.ios'].unshift('react-native-webpack-server/hot/entry');
-  config.entry['index.ios'].unshift('webpack/hot/only-dev-server');
-  config.entry['index.ios'].unshift('webpack-dev-server/client?http://localhost:8082');
+  config.devtool = 'source-map';
+  config.entry['index.ios'].unshift(
+    'react-native-webpack-server/hot/entry',
+    'webpack-hot-middleware/client?path=http://localhost:8082/__webpack_hmr&overlay=false'
+  );
   config.output.publicPath = 'http://localhost:8082/';
-  config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+  config.plugins.unshift(
+    new webpack.optimize.OccurenceOrderPlugin(), 
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  );
+  config.output.publicPath = 'http://localhost:8082/';
   config.module.loaders[0].query.plugins.push('react-transform');
   config.module.loaders[0].query.extra = {
     'react-transform': {
