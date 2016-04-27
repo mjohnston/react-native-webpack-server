bb719f43c85b21f07a8f31251828c919a4d0061f
YuTin Liu <yuting1987@gmail.com>
2016-04-27 10:14:12 +0800

    fix(examples): react-transform error in webpack.config.js

--- Examples/BabelES6/webpack.config.js
+++ Examples/BabelES6/webpack.config.js
@@ -30,6 +30,7 @@ var config = {
       loader: 'babel',
       query: {
         presets: ['es2015', 'stage-0', 'react'],
+        plugins: [],
       },
     }]
   },
@@ -46,16 +47,17 @@ if (process.env.HOT) {
   config.entry['index.ios'].unshift('webpack-dev-server/client?http://localhost:8082');
   config.output.publicPath = 'http://localhost:8082/';
   config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
-  config.module.loaders[0].query.plugins.push('react-transform');
-  config.module.loaders[0].query.extra = {
-    'react-transform': {
+
+  // Note: enabling React Transform and React Transform HMR:
+  config.module.loaders[0].query.plugins.push([
+    'react-transform', {
       transforms: [{
         transform: 'react-transform-hmr',
         imports: ['react-native'],
         locals: ['module']
       }]
     }
-  };
+  ]);
 }
 
 // Production config
